7ed8453e18b4a7a7129d7dd4a45a0ef524d84ccf
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-01-13 20:20:03 -0500

    starter API

---
+++ script.js
@@ -0,0 +1,12 @@
+var APIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
+var queryURL = "https://api.coinlayer.com/live"
++ "?access_key=" + APIkey ;
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
