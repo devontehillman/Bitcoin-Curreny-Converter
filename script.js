2700fde6ed2dd2727602d327eb3912a511066228
Andrew Chappell <chappand@gmail.com>
2021-01-13 20:36:13 -0500

    Console logging live BTC price.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var APIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
-var queryURL = "https://api.coinlayer.com/live"
-+ "?access_key=" + APIkey ;
-
+var liveQueryURL = "http://api.coinlayer.com/live"
++ "?access_key=" + APIkey;
 
 
 $.ajax({
-    url: queryURL,
+    url: liveQueryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
+    console.log(response.rates.BTC);
   });
+
  
