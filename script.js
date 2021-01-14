7a6fe50da2fc97cbacb88247ccae803472f3d6fb
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-01-13 20:57:21 -0500

    included currency converter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var APIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
-var queryURL = "https://api.coinlayer.com/live"
+var queryURL = "http://api.coinlayer.com/live"
 + "?access_key=" + APIkey ;
 
 
@@ -8,6 +8,13 @@ $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
+    console.log(response.rates.BTC);
   });
  
+ 
+  $.ajax({
+    url: "https://api.exchangeratesapi.io/latest??symbols=USD,EUR",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response.rates.USD);
+  });
