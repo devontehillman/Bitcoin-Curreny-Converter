87715aed9c627ed0924aded60a7d5fbad3f42f38
Andrew Chappell <chappand@gmail.com>
2021-01-13 20:52:08 -0500

    Tested out Conversion APIs.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var APIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
+var liveAPIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
 var liveQueryURL = "http://api.coinlayer.com/live"
-+ "?access_key=" + APIkey;
-
++ "?access_key=" + liveAPIkey;
 
 $.ajax({
     url: liveQueryURL,
@@ -9,5 +8,3 @@ $.ajax({
   }).then(function(response) {
     console.log(response.rates.BTC);
   });
-
- 
