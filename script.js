968779abb8c3f3a0072a2198296f6bde22842885
Andrew Chappell <chappand@gmail.com>
2021-01-16 11:05:23 -0500

    Added functionality to get the value of the dropdown when the convert button is clicked.

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 var liveAPIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
 var liveQueryURL = "http://api.coinlayer.com/live"
 + "?access_key=" + liveAPIkey;
-var rateQueryURL = "https://api.exchangeratesapi.io/latest??symbols=USD," + formInput;
+var rateQueryURL = "https://api.exchangeratesapi.io/latest??symbols=USD,EUR";
+
+$("button").click(function() {
+  var selection = $("select").val();
+  console.log(selection);
+
+  var liveAPIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
+  var liveQueryURL = "http://api.coinlayer.com/live"
+  + "?access_key=" + liveAPIkey;
+  var rateQueryURL = "https://api.exchangeratesapi.io/latest??symbols=USD," + selection;
 
 $.ajax({
     url: liveQueryURL,
@@ -18,4 +27,8 @@ $.ajax({
   }).then(function(response) {
     console.log(response.rates.USD);
   });
+
+
+
+});
 
