72f0ea23a530f875e72b0ed0977fbbfbbbaf7924
Andrew Chappell <chappand@gmail.com>
2021-01-16 13:01:28 -0500

    created function to calculate the actualy exchage rate between currencies.

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
 
-var liveAPIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
-var liveQueryURL = "http://api.coinlayer.com/live"
-+ "?access_key=" + liveAPIkey;
-var rateQueryURL = "https://api.exchangeratesapi.io/latest??symbols=USD,EUR";
 
 $("button").click(function() {
+  // var ratesArray = [""];
   var selection = $("select").val();
   console.log(selection);
 
   var liveAPIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
   var liveQueryURL = "http://api.coinlayer.com/live"
   + "?access_key=" + liveAPIkey;
-  var rateQueryURL = "https://api.exchangeratesapi.io/latest??symbols=USD," + selection;
+  var rateQueryURL = "https://api.exchangeratesapi.io/latest?base=USD&symbols=USD," + selection;
+
+
+  function exchangeRate() {
+
+    $.ajax({
+      url: rateQueryURL,
+      method: "GET"
+    }).then(function(response) {
+      var selectionRates = response.rates[selection];
+      var USDRates = response.rates.USD;
+      function convert() {
+        var anything = selectionRates/USDRates;
+        return anything;
+      };
+      console.log(convert());
+      $(".selectedCurrency").text(selection);
+  
+    });
+    };
+
 
   $.ajax({
     url: liveQueryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response.rates.BTC);
-  });
- 
- 
-  $.ajax({
-    url: rateQueryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response.rates.USD);
+    var BTCRate = response.rates.BTC;
+    console.log(BTCRate);
+    exchangeRate();
+    var final = anything * BTCRate;
+    console.log(final);
+
   });
 
+
+  // function exchangeRate() {
+
+  // $.ajax({
+  //   url: rateQueryURL,
+  //   method: "GET"
+  // }).then(function(response) {
+  //   var selectionRates = response.rates[selection];
+  //   var USDRates = response.rates.USD;
+  //   function convert() {
+  //     (selectionRates/USDRates) * BTCRate
+  //   };
+  //   console.log(convert());
+  //   $(".selectedCurrency").text(selection);
+
+  // });
+  // };
 
 
 });
