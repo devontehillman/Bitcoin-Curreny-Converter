f4e13f9c1ad5a646f8a9b152158351a03e607a9a
Andrew Chappell <chappand@gmail.com>
2021-01-20 20:44:25 -0500

    Added comments to the script and edited the Readme.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Bitcoin-Curreny-Converter
+
+## Description
+Here our team has created a bitcoin currency converter which takes the current bitcoin in usd and converts it to bitcoin in the user's selcted currency([Visit the website here.](finshed webpage url)). Our websites features include:
+* Current bitcoin conversion to selected currency.   
+* Inspirational Kanye qoute. 
+
+## Finished-Page
+![Refactored Webpage](image of finished webpage)
+
+## What did we learn?
+In this project we learned:
+* How to work as a team to create and complete a project.
+* How to collaborate on a project through github. 
+* How to Create a Kanban Board for productivity managment.
+* How to call a dynamic property from API based on user selection.
+* How to call an item selected from a drop down and get it's attributes
+
+
+## Future Improvments
+*
--- script.js
+++ script.js
@@ -1,50 +1,60 @@
 
-
-$("button").click(function() {
+// This is the click function that kicks off all of the functionality and updates
+$("button").click(function () {
+  // The selection variable holds the user's currency selection so we can call on it later
   var selection = $("select").val();
   console.log(selection);
 
+  // These variables house our URLS and API Keys for our AJAX queries
   var liveAPIkey = "0f0c1950be32c8fb3bf766b86a8c9b34";
   var liveQueryURL = "http://api.coinlayer.com/live"
-  + "?access_key=" + liveAPIkey;
+    + "?access_key=" + liveAPIkey;
   var rateQueryURL = "https://api.exchangeratesapi.io/latest?base=USD&symbols=USD," + selection;
   var kanyeAPI = "https://api.kanye.rest";
 
+  // This ajax function is a GET method to get a quote from the "Kanye Rest" API
   $.ajax({
     url: kanyeAPI,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(response);
+    // This is where we append the quote we retrieved using the "Kanye Rest" API
     $(".Kanye").text("Kanye inspirational quote:").append("<br>").append("<h1>" + "'" + response.quote + "'");
-
   });
 
+  // This ajax function is a GET method to get the exchage rate of the specified currency
   $.ajax({
-      url: rateQueryURL,
-      method: "GET"
-    }).then(function(response) {
-      var selectionRates = response.rates[selection];
-      var USDRates = response.rates.USD;
-      var anything = selectionRates/USDRates;
-      console.log(anything);
-      $(".selectedCurrency").text(selection);
-      localStorage.setItem("exchangeRate", anything);
-    });
+    url: rateQueryURL,
+    method: "GET"
+  }).then(function (response) {
+    // These three variables hold the Rate of the selected currency in EUR, the USD currency in EUR and the selected currency in USD
+    var selectionRates = response.rates[selection];
+    var USDRates = response.rates.USD;
+    var anything = selectionRates / USDRates;
+    console.log(anything);
+    // This is where we added text to the HTML showing what the user selected
+    $(".selectedCurrency").text(selection);
+    // We stored the selected currency rate in USD in local storage so we could call it later in a seperate ajax function
+    localStorage.setItem("exchangeRate", anything);
+  });
 
-
+  // This ajax function is a GET method to get the current Bitcoin rate
   $.ajax({
-      url: liveQueryURL,
-      method: "GET"
-    }).then(function(response) {
-      var BTCRate = response.rates.BTC;
-      console.log(BTCRate);
-      var something = localStorage.getItem("exchangeRate");
-      var final = something * BTCRate;
-      var rounded = Math.round(final * 100)/100;
-      Number.parseFloat(rounded).toFixed(2);
-      $(".BTC-Conversion").text("$" + rounded);
+    url: liveQueryURL,
+    method: "GET"
+  }).then(function (response) {
+    // These variables hole the current Bitcoin rate, the selected currency rate in USD that we pulled from local storage, The selected currency rate in Bitcoin, and the selected currency rate in Bitcoin
+    var BTCRate = response.rates.BTC;
+    console.log(BTCRate);
+    var something = localStorage.getItem("exchangeRate");
+    var final = something * BTCRate;
+    var rounded = Math.round(final * 100) / 100;
+    // This is to format the rate to #,###.##
+    Number.parseFloat(rounded).toFixed(2);
+    // This is to add the text to the HTML to show the final converted price of the selected currency in Bitcoin
+    $(".BTC-Conversion").text("$" + rounded);
 
-    });
+  });
 
 
 });
